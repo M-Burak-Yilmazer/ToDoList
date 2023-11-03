1b2f8b593bf848c103d95b4e829f9e165ac33f8f
mathiasburger <mburakyilmazer38@gmail.com>
2023-11-03 21:54:38 +0300

    images

--- app.js
+++ app.js
@@ -5,11 +5,33 @@ const mainList = document.getElementById("list");
 const listArray = [];
 
 addButton.addEventListener("click", (e) => {
-  if (!listArray.includes(inputVal.value)) {
-    listArray.push(inputVal.value);
-    const listPart = document.createElement("li");
-    const text = document.createTextNode(inputVal.value);
-    listPart.appendChild(text);
-    mainList.appendChild(listPart);
+  if (!(inputVal.value.trim() === "")) {
+    if (!listArray.includes(inputVal.value)) {
+      listArray.push(inputVal.value);
+      const listPart = document.createElement("li");
+      const text = document.createTextNode(inputVal.value.toUpperCase());
+      listPart.appendChild(text);
+      mainList.appendChild(listPart);
+      inputVal.value = "";
+      inputVal.focus();
+    }
+  }
+});
+
+inputVal.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    addButton.click();
+  }
+});
+
+removeButton.addEventListener("click", (e) => {
+  listArray.pop();
+  mainList.removeChild(mainList.lastElementChild);
+});
+inputVal.addEventListener("keydown", (e) => {
+  if (e.key === "Backspace") {
+    if (inputVal.value.trim() === "") {
+      removeButton.click();
+    }
   }
 });
